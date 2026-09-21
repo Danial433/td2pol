52b290f3d27a33401dfec38d6d664509d5841190
sira <sirajuddinfaris06@gmail.com>
2026-09-21 15:26:55 +0200

    hey

---
+++ src/Biblio.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+public class Biblio {
+    public String nom;
+    public ArrayList<Livre>livres;
+    
+    public Biblio(String nom) {
+        this.nom = nom;
+        this.livres = new ArrayList<>();
+    }
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void ajouteLivre(Livre nom){
+        livres.add(nom);
+    }
+    public void afficherLivres(){
+        System.out.print("Livres dans la bibliotheque " + livres);
+    }
+    public 
+}
