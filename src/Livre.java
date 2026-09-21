8176a8142709124b0fcb62cbd45c1ab26f1f5191
Jiku <danialaqil433@gmail.com>
2026-09-21 14:44:20 +0200

    Updated Livre

--- src/Livre.java
+++ src/Livre.java
@@ -1,11 +1,47 @@
-import java.io.Serializable;
 import java.util.ArrayList;
 
 public abstract class Livre {
-    private String title;
+    private String titre;
     private String auteur;
     private String isbn;
-    private String ArrayList<Examplaire> exemplaires;
+    private ArrayList<Exemplaire> exemplaires;
 
-    public 
+    public Livre(String titre, String auteur, String isbn) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.isbn = isbn;
+        this.exemplaires = new ArrayList<>();
+
+    }
+
+    public String getTitre(){
+        return titre;
+    }
+
+    public void setTitre(String titre){
+        this.titre = titre;
+    
+    }
+
+    public String getAuteur(){
+        return auteur;
+
+    }
+
+    public void setAuteur(String auteur){
+        this.auteur = auteur;
+
+    }
+
+    public String getIsbn(){
+        return isbn;
+    }
+
+    public void setIsbn(String isbn){
+        this.isbn = isbn;
+    }
+
+    public Arraylist<Exemplaire> getExemplaire {
+        
+    }
 }
