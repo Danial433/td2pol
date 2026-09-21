dc0ac525d18c786cc93d534b3f22ba984c10ad96
Jiku <danialaqil433@gmail.com>
2026-09-21 14:28:20 +0200

    First commit

---
+++ src/Livre.java
@@ -0,0 +1,11 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public abstract class Livre {
+    private String title;
+    private String auteur;
+    private String isbn;
+    private String ArrayList<Examplaire> exemplaires;
+
+    public 
+}
