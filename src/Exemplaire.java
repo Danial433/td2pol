85e8235dba7fd9cc53cb19d1c4c453e40b4ea57a
Jiku <danialaqil433@gmail.com>
2026-09-21 15:09:58 +0200

    Completed Exemplaire

--- src/Exemplaire.java
+++ src/Exemplaire.java
@@ -1,3 +1,43 @@
-public class Exemplaire {
+mport java.io.Serializable;
 
+public class Exemplaire implements Serializable {
+
+    private String emplacement;
+    private String etat;
+
+    public Exemplaire(String emplacement, String etat) {
+        this.emplacement = emplacement;
+        this.etat = etat;
+    }
+
+    public String getEmplacement() {
+        return emplacement;
+    }
+
+    public String getEtat() {
+        return etat;
+    }
+
+    public void setEmplacement(String emplacement) {
+        this.emplacement = emplacement;
+    }
+
+    public void setEtat(String etat) {
+        this.etat = etat;
+    }
+
+    public void afficher() {
+        System.out.println("Emplacement : " + emplacement);
+        System.out.println("Etat : " + etat);
+    }
+
+    public void preter() {
+        if (etat.equals("disponible")) {
+            etat = "prêté";
+        }
+    }
+
+    public void rendre() {
+        etat = "disponible";
+    }
 }
