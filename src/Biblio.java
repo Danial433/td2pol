8e5f0a67ea2126bc60fe5a417c6553ee33580ed1
Jiku <danialaqil433@gmail.com>
2026-09-25 13:53:24 +0200

    JSON sauvergarde

--- src/Biblio.java
+++ src/Biblio.java
@@ -3,8 +3,13 @@ import java.io.FileOutputStream;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.io.FileWriter;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-public class Biblio {
+
+public class Biblio implements Serializable {
     public String nom;
     public ArrayList<Livre>livres;
     
@@ -91,5 +96,104 @@ public class Biblio {
         }
     }
 
+
+    public void sauvergarder(String fichier){
+        try {
+            FileOutputStream fos = new FileOutputStream (fichier);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+
+            oos.writeObject(this);
+
+            oos.close();
+            fos.close();
+
+            System.out.println("Bibiliotheque sauvergardée");
+
+
+        } catch (Exception e) {
+            System.out.println("Erreur lors de la sauvergarde");
+        }
+
+    }
+
+        public static Biblio charger(String fichier){
+            try {
+                FileInputStream fis = new FileInputStream(fichier);
+                ObjectInputStream ois = new ObjectInputStream(fis);
+
+                Biblio biblio  = (Biblio) ois.readObject();
+
+                ois.close();
+                fis.close();
+
+
+                System.out.println("Bibliotheque chargée");
+
+                return biblio;
+
+
+            } catch (Exception e) {
+            System.out.println("Erreur lors de la sauvergarde");
+            } 
+
+            return null;
+
+        }
+
+        public void sauvergarderJSON(String fichier){
+            try{
+                JSONObject objetBiblio = new JSONObject();
+
+                objetBiblio.put("nom", nom);
+
+                JSONArray tableauLivres = new JSONArray();
+
+                for(Livre livre : livres) {
+                    JSONObject objetLivre = new JSONObject();
+
+                    objetLivre.put("titre",livre.getTitre());
+                    objetLivre.put("auteur",livre.getAuteur());
+                    objetLivre.put("ISBN",livre.getIsbn());
+
+                    if (livre instanceof Roman) {
+                        objetLivre.put("genre","Roman");
+
+                    } else if (livre instanceof BD){
+                        objetLivre.put("dessinateur",((BD)livre).getDessinateur());
+
+                    } else if (livre instanceof Journal){
+                        objetLivre.put("numeroParution",((Journal)livre).getNumeroParution());
+                    }
+
+                    JSONArray tableauExemplaires = new JSONArray();
+
+                    for(Exemplaire exemplaire : livre.getExemplaires()){
+                        JSONObject objetExemplaire = new JSONObject();
+
+                        objetExemplaire.put("emplacement", exemplaire.getEmplacement());
+                        objetExemplaire.put("etat", exemplaire.getEtat());
+
+                        tableauExemplaires.put(objetExemplaire);
+
+                        objetLivre.put("exemplaires", tableauExemplaires);
+                    }
+
+                    tableauLivres.put(objetLivre);
+
+                }
+
+                objetBiblio.put("livres", tableauLivres);
+
+                FileWriter fichierJSON = new FileWriter(fichier);
+
+                fichierJSON.write(objetBiblio.toString());
+
+                fichierJSON.close();
+
+            } catch (Exception e) {
+                System.out.println("erreur lors de la sauvergarde");
+            }
+        }
 
 }
